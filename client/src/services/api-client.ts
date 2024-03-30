4d13a8bdf27c4c0f1fa09c29b559c668692d5b71
Howard Liu <zhihliu@stonybrook.edu>
2024-03-30 09:59:58 -0400

    change the front-and and back-end baseURL for deployment

--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 
 export default axios.create({
-    baseURL: 'http://localhost:4000'
+    baseURL: 'https://care-flow-2024-5ef1aadf128e.herokuapp.com/'
 })
--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://care-flow.vercel.app/"],
     credentials: true
 }))
 

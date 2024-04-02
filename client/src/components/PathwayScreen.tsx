9d9a153de59600ad5de64a2a4d7317caa5b9d799
Sabrina Zheng <szheng@Sabrinas-MacBook-Pro.local>
2024-04-02 13:28:17 -0400

    navbar edit

--- client/src/components/PathwayScreen.tsx
+++ client/src/components/PathwayScreen.tsx
@@ -2,7 +2,6 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import SideBar from "./SideBar";
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,

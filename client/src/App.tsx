ed3f1831706aa14fe1bfd434fff4f518c99c2289
Sabrina Zheng <szheng@Sabrinas-MacBook-Pro.local>
2024-04-02 12:54:48 -0400

    navbar,sidebar,splashscreen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,37 @@
 import { useEffect, useState } from "react";
 import userService, { User } from "./services/user-service";
+import PathwayScreen from "./components/PathwayScreen";
+import SideBar from "./components/SideBar";
+import NavBar from "./components/NavBar";
+import Splash from "./components/Splash";
+import SplashScreen from "./components/SplashScreen";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
 
-  useEffect(() => {
-    userService.getAll<User>().then((res) => {
-      setUsers(res.data);
-    }).catch((err) => {
-      console.log(err);
-    })
-  }, []);
+    useEffect(() => {
+        userService
+            .getAll<User>()
+            .then((res) => {
+                setUsers(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-  return <>
-    <div>Hello Care Flow</div>
-    {users.map(user => <li key={user.id}>{user.name}</li>)}
-  </>
+    return (
+        <div id="app">
+            {/* <div>Hello Care Flow</div> */}
+            {/* {users.map((user) => (
+                <li key={user.id}>{user.name}</li>
+            ))} */}
+            {/* <PathwayScreen /> */}
+            {/* <NavBar />
+            <SideBar /> */}
+            <SplashScreen />
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ client/src/components/SideBar.tsx
@@ -0,0 +1,59 @@
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import ListIcon from "@mui/icons-material/List";
+import GroupIcon from "@mui/icons-material/Group";
+import EventAvailableIcon from "@mui/icons-material/EventAvailable";
+import { Divider, Toolbar } from "@mui/material";
+
+export default function SideBar() {
+    const drawerWidth = 240;
+    const resourceTabs = [
+        { icon: <LocalHospitalIcon />, name: "Resource" },
+        { icon: <AssignmentIcon />, name: "Pathway" },
+        { icon: <ListIcon />, name: "Template" },
+        { icon: <MailIcon />, name: "Message" },
+    ];
+    const caregiverTabs = [
+        { icon: <EventAvailableIcon />, name: "Schedule" },
+        { icon: <MailIcon />, name: "Message" },
+    ];
+    const adminTabs = [
+        { icon: <GroupIcon />, name: "Accounts" },
+        { icon: <MailIcon />, name: "Message" },
+    ];
+
+    return (
+        <Drawer
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                "& .MuiDrawer-paper": {
+                    width: drawerWidth,
+                    boxSizing: "border-box",
+                },
+            }}
+            variant="permanent"
+            anchor="left"
+        >
+            <Toolbar />
+            <List>
+                {resourceTabs.map(({ icon, name }, index) => (
+                    <ListItem key={index} disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>{icon}</ListItemIcon>
+                            <ListItemText primary={name} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    );
+}

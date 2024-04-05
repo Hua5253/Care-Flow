0e22cd42aafefd16b9f572e683c79a7ee62d8fe9
Howard <zhihua.liu@stonybrook.edu>
2024-04-05 12:08:42 -0400

    add message route

--- client/src/routers.tsx
+++ client/src/routers.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import SplashScreen from "./components/SplashScreen";
 import PathwayScreen from "./components/pathwaysTab/pathwayScreen";
 import ProcedureScreen from "./components/pathwaysTab/procedureScreen";
+import MessageTab from "./components/MessagesTab";
 
 const router = createBrowserRouter([
     {path: '/', element: <SplashScreen />},
     {path: '/manager-pathway', element: <PathwayScreen />},
-    {path: '/manager-procedure', element: <ProcedureScreen />}
+    {path: '/manager-procedure', element: <ProcedureScreen />},
+    {path: '/message', element: <MessageTab />}
 ])
 
 export default router;

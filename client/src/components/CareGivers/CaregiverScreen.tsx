b3d3a2e2987e5b45b68532ca49b548b86ac2afa0
KangQiChen <kangqi.chen@stonybrook.edu>
2024-04-05 13:02:36 -0400

    fixed margin top

--- client/src/components/CareGivers/CaregiverScreen.tsx
+++ client/src/components/CareGivers/CaregiverScreen.tsx
@@ -3,6 +3,7 @@ import { Container } from "@mui/material";
 import AppBanner from "../AppBanner/AppBanner";
 import ViewProcedure from "./ViewProcedure";
 
+
 export default function CaregiverScreen() {
   return (
     <Container sx={{ display: "flex" }}>

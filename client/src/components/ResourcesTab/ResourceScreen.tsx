fc57339dd58e06069c6de76f567235be35078355
Howard <zhihua.liu@stonybrook.edu>
2024-04-05 14:02:56 -0400

    create pathway template screen

--- client/src/components/ResourcesTab/ResourceScreen.tsx
+++ client/src/components/ResourcesTab/ResourceScreen.tsx
@@ -1,11 +1,13 @@
 import { Container } from "@mui/material";
 import AppBanner from "../AppBanner/AppBanner";
 import Resources from "./Resources";
+import ManagerSideBar from "../SideBar/ManagerSideBar";
 
 export default function ResourceScreen() {
   return (
     <Container id="resourceScreen" sx={{ display: "flex" }}>
-      <AppBanner cred={true} section={"Resource"} />
+      <ManagerSideBar />
+      <AppBanner cred={true} />
       <Resources />
     </Container>
   );

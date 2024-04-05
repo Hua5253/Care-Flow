cd103ca847fa2cccae84ed55350c924465f04384
KangQiChen <kangqi.chen@stonybrook.edu>
2024-04-04 23:39:37 -0400

    added popup modal

---
+++ client/src/components/ResourceModals/PopupModal.tsx
@@ -0,0 +1,70 @@
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import { maxWidth } from '@mui/system';
+
+const modalStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+};
+  
+const paperStyle = {
+    backgroundColor: '#5C6B73',
+    boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
+    padding: '20px',
+    color: '#ffffff',
+    textAlign: 'center',
+    fontSize: '1.5rem',
+};
+
+const buttonContainerStyle = {
+    marginTop: '20px',
+    display: 'flex',
+    justifyContent: 'space-around',
+};
+
+export default function PopupModal() {
+
+    let procedure_name = "MRI"
+    let patient_name = "john doe"
+    let start_procedure = "Click confirm to start the procedure"
+    let end_procedure = "Click confirm to end the procedure"
+    let cancel_procedure = "Click confirm to cancel the procedure"
+
+    function handleConfirm() {
+        console.log("confirm pressed");
+    }
+
+    function handleCancel() {
+        console.log("cancel pressed");
+    }
+
+
+    let body = "Lorem ipsum dolor sit amet, consectetur"
+
+
+    return (
+    <Modal
+        open={true}
+        style={modalStyle}
+        aria-labelledby="confirmation-modal-title"
+        aria-describedby="confirmation-modal-description"
+    >
+        <Box sx={paperStyle}>
+            <div >
+            <Box>
+                <h3 id="confirmation-modal-description">{body}</h3>
+            </Box>
+                <div style={buttonContainerStyle}>
+                    <Button onClick={handleConfirm} variant="contained" color="primary" style={{ backgroundColor: '#253237', color: '#ffffff' }}>
+                        Confirm
+                    </Button>
+                </div>
+                
+            </div>
+        </Box>
+    </Modal>
+    
+    );
+}

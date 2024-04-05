5e456fd436d656d88516a17179d6a1c83189b41e
Howard Liu <zhihua.liu@stonybrook.edu>
2024-04-05 12:01:59 -0400

    add editProcedureModal

--- client/src/components/pathwaysTab/modals/CreatePathwayModal.tsx
+++ client/src/components/pathwaysTab/modals/CreatePathwayModal.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Modal, Typography } from "@mui/material";
+import { Box, Button, Modal } from "@mui/material";
 import { useState } from "react";
 import TemplatePathwayModal from "./TemplatePathwayModal";
 import IconButton from "@mui/material/IconButton";
--- client/src/components/pathwaysTab/procedureScreen/InEditFooterButtons.tsx
+++ client/src/components/pathwaysTab/procedureScreen/InEditFooterButtons.tsx
@@ -2,12 +2,16 @@ import { Box, Button } from "@mui/material";
 
 interface Props {
   handleSaveClick: () => void;
+  handleAddProcedure: () => void;
 }
 
-const InEditFooterButtons = ({ handleSaveClick }: Props) => {
+const InEditFooterButtons = ({
+  handleSaveClick,
+  handleAddProcedure,
+}: Props) => {
   return (
     <Box display="flex" justifyContent="space-between" padding={2}>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={handleAddProcedure}>
         Add
       </Button>
       <Box sx={{ display: "flex", gap: 1 }}>

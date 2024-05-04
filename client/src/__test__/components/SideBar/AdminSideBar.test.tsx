5eadc4683d7c4edb43ed2e19f2c484c42742d423
sdsdc123 <sicheng.liu@stonybrook.edu>
2024-05-04 13:11:40 +0800

    raise client test coverage

--- client/src/__test__/components/SideBar/AdminSideBar.test.tsx
+++ client/src/__test__/components/SideBar/AdminSideBar.test.tsx
@@ -1,26 +1,66 @@
-import { Router } from "react-router-dom";
-import { createMemoryHistory } from "history";
-import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import AdminSideBar from "../../../components/SideBar/AdminSideBar";
 
-describe("AdminSideBar Component", () => {
-  beforeEach(() => {
-    const history = createMemoryHistory({ initialEntries: ["/"] });
-    render(
-      <Router location={history.location} navigator={history}>
-        <AdminSideBar />
-      </Router>
-    );
-  });
-  it("should render all sidebar items and navigate when clicked", () => {
-    const accountsSidebarItem = screen.getByTestId(/Accounts/i);
-    const messageSidebarItem = screen.getByTestId(/Message/i);
-    expect(accountsSidebarItem).toBeInTheDocument();
-    expect(messageSidebarItem).toBeInTheDocument();
-    // fireEvent.click(accountsSidebarItem);
-    // expect(window.location.pathname).toBe("/accounts");
+jest.mock("@mui/material/useMediaQuery");
 
-    // fireEvent.click(messageSidebarItem);
-    // expect(window.location.pathname).toBe("/Messages/admin");
+describe("AdminSideBar Component", () => {
+  it("should render all sidebar items and navigate when clicked", () => {
+    const { getByTestId, container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <AdminSideBar />
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route
+            path="/accounts"
+            element={<div className="AccountsPage">Accounts Page</div>}
+          />
+          <Route
+            path="/Messages/admin"
+            element={
+              <div className="MessagesAdminPage">Messages Admin Page</div>
+            }
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+    let pageContent = container.querySelector(".AccountsPage");
+    expect(pageContent).not.toBeInTheDocument();
+
+    const accountsSidebarItem = getByTestId("Accounts");
+    userEvent.click(accountsSidebarItem);
+
+    pageContent = container.querySelector(".AccountsPage");
+    expect(pageContent).toBeInTheDocument();
+
+    const messageSidebarItem = getByTestId("Message");
+    userEvent.click(messageSidebarItem);
+
+    pageContent = container.querySelector(".MessagesAdminPage");
+    expect(pageContent).toBeInTheDocument();
+  });
+  it("should set drawer width to 0 when between xs and md", () => {
+    const useMediaQuery = require("@mui/material/useMediaQuery").default;
+    useMediaQuery.mockImplementation(() => true);
+    const { container } = render(
+      <MemoryRouter>
+        <AdminSideBar />
+      </MemoryRouter>
+    );
+    const drawer = container.querySelector(".MuiDrawer-paper");
+    expect(drawer).toHaveStyle("width: 0px");
+  });
+
+  it("should set drawer width to 240 when not between xs and md", () => {
+    const useMediaQuery = require("@mui/material/useMediaQuery").default;
+    useMediaQuery.mockImplementation(() => false);
+    const { container } = render(
+      <MemoryRouter>
+        <AdminSideBar />
+      </MemoryRouter>
+    );
+    const drawer = container.querySelector(".MuiDrawer-paper");
+    expect(drawer).toHaveStyle("width: 240px");
   });
 });
--- client/src/__test__/components/SideBar/ManagerSideBar.test.tsx
+++ client/src/__test__/components/SideBar/ManagerSideBar.test.tsx
@@ -1,30 +1,85 @@
-import { Router } from "react-router-dom";
-import { createMemoryHistory } from "history";
-import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ManagerSideBar from "../../../components/SideBar/ManagerSideBar";
 
-describe("ManagerSideBar Component", () => {
-  beforeEach(() => {
-    const history = createMemoryHistory({ initialEntries: ["/"] });
-    render(
-      <Router location={history.location} navigator={history}>
-        <ManagerSideBar />
-      </Router>
-    );
-  });
-  it("should render all sidebar items and navigate when clicked", () => {
-    const resourceSidebarItem = screen.getByTestId(/Resource/i);
-    const pathwaySidebarItem = screen.getByTestId(/Pathway/i);
-    const templateSidebarItem = screen.getByTestId(/Template/i);
-    const messageSidebarItem = screen.getByTestId(/Message/i);
-    expect(resourceSidebarItem).toBeInTheDocument();
-    expect(pathwaySidebarItem).toBeInTheDocument();
-    expect(templateSidebarItem).toBeInTheDocument();
-    expect(messageSidebarItem).toBeInTheDocument();
-    // fireEvent.click(accountsSidebarItem);
-    // expect(window.location.pathname).toBe("/accounts");
+jest.mock("@mui/material/useMediaQuery");
 
-    // fireEvent.click(messageSidebarItem);
-    // expect(window.location.pathname).toBe("/Messages/admin");
+describe("ManagerSideBar Component", () => {
+  it("should render all sidebar items and navigate when clicked", () => {
+    const { getByTestId, container } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <ManagerSideBar />
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route
+            path="/resources"
+            element={<div className="ResourcePage">Resource Page</div>}
+          />
+          <Route
+            path="/manager-pathway"
+            element={<div className="PathwayPage">Pathway Page</div>}
+          />
+          <Route
+            path="/manager-template"
+            element={<div className="TemplatePage">Template Page</div>}
+          />
+          <Route
+            path="/Messages/manager"
+            element={<div className="MessagePage">Message Page</div>}
+          />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    let pageContent = container.querySelector(".ResourcePage");
+    expect(pageContent).not.toBeInTheDocument();
+
+    const resourceSidebarItem = getByTestId("Resource");
+    userEvent.click(resourceSidebarItem);
+
+    pageContent = container.querySelector(".ResourcePage");
+    expect(pageContent).toBeInTheDocument();
+
+    const pathwaySidebarItem = getByTestId("Pathway");
+    userEvent.click(pathwaySidebarItem);
+
+    pageContent = container.querySelector(".PathwayPage");
+    expect(pageContent).toBeInTheDocument();
+
+    const templateSidebarItem = getByTestId("Template");
+    userEvent.click(templateSidebarItem);
+
+    pageContent = container.querySelector(".TemplatePage");
+    expect(pageContent).toBeInTheDocument();
+
+    const messageSidebarItem = getByTestId("Message");
+    userEvent.click(messageSidebarItem);
+
+    pageContent = container.querySelector(".MessagePage");
+    expect(pageContent).toBeInTheDocument();
+  });
+  it("should set drawer width to 0 when between xs and md", () => {
+    const useMediaQuery = require("@mui/material/useMediaQuery").default;
+    useMediaQuery.mockImplementation(() => true);
+    const { container } = render(
+      <MemoryRouter>
+        <ManagerSideBar />
+      </MemoryRouter>
+    );
+    const drawer = container.querySelector(".MuiDrawer-paper");
+    expect(drawer).toHaveStyle("width: 0px");
+  });
+
+  it("should set drawer width to 240 when not between xs and md", () => {
+    const useMediaQuery = require("@mui/material/useMediaQuery").default;
+    useMediaQuery.mockImplementation(() => false);
+    const { container } = render(
+      <MemoryRouter>
+        <ManagerSideBar />
+      </MemoryRouter>
+    );
+    const drawer = container.querySelector(".MuiDrawer-paper");
+    expect(drawer).toHaveStyle("width: 240px");
   });
 });

281647879603feba1f579940c9dcb3d5677dfcbc
sdsdc123 <sicheng.liu@stonybrook.edu>
2024-05-13 16:33:31 +0800

    docker nginx version change

--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://3.142.237.164:4000' : "http://localhost:4000",
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://3.142.237.164:4000' : "http://localhost:4000",
   withCredentials: true
 });

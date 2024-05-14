9a32a89d10caa7b4bb8230828c1809438101cc49
sdsdc123 <sicheng.liu@stonybrook.edu>
2024-05-14 14:25:36 +0800

    docker-compose localhost test success try aws

--- client/src/services/api-client.ts
+++ client/src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://18.222.46.211:4000' : "http://localhost:4000",
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://localhost/api' : "http://localhost:4000",
   withCredentials: true
 });
--- server/index.ts
+++ server/index.ts
@@ -17,7 +17,7 @@ dotenv.config();
 interface UserSocket extends Socket {
   userId?: string;
 }
-const origin = ['test', 'dev'].includes(process.env.NODE_ENV || '') ? "http://localhost:5173" : "https://18.222.46.211";
+const origin = ['test', 'dev'].includes(process.env.NODE_ENV || '') ? "http://localhost:5173" : ["https://localhost", 'http://172.18.208.1', 'http://18.222.46.211'];
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {

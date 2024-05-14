9294b0e73e33765a7badef1d877fcdb70d6d80b5
sdsdc123 <sicheng.liu@stonybrook.edu>
2024-05-14 15:00:22 +0800

    socket origin change to *

--- server/index.ts
+++ server/index.ts
@@ -22,7 +22,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin
+    origin: '*'
   }
 });
 const onlineUsers: Record<string, string> = {};
